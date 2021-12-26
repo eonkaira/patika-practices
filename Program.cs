aa8b6e5d392b5f7a18c3591fbcfd3657952fc400
Emre Ozden <eokaira@users.noreply.github.com>
2021-12-27 01:22:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace patika_practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name:");
+            var name=Console.ReadLine();
+            Console.WriteLine("Enter your surname:");
+            var surname=Console.ReadLine();
+            Console.WriteLine("Welcome "+name+" "+surname);
+        }
+    }
+}
