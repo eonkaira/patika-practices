f9d94f41bf5819aa70a552fdd8373e61f0fc55e2
Emre Ozden <emreozden.dev@gmail.com>
2022-01-10 12:40:44 +0300

    For Loop added

--- Program.cs
+++ Program.cs
@@ -277,7 +277,7 @@ namespace patika_practices
 
             //---- SWITCH-CASE ----
             //* Expression
-            int month = DateTime.Now.Month;
+            /* int month = DateTime.Now.Month;
             switch (month)
             {
                 case 1:
@@ -310,7 +310,59 @@ namespace patika_practices
                 
                 default:
                     break;
+            } */
+
+            //---- FOR LOOP ----
+            //* Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i<=sayac; i++)
+            {
+                if(i%2 == 1)
+                    Console.WriteLine(i);
             }
+
+            //* 1 ile 1000  arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır
+            int tekToplam=0;
+            int ciftToplam=0;
+
+            for (int i = 1; i <= 1000 ; i++)
+            {
+                if(i%2==1)
+                    tekToplam += i; //* tekToplam = tekToplam + i
+
+                else
+                    ciftToplam += i;
+            }
+
+            Console.WriteLine("Tek Toplam: "+tekToplam);
+            Console.WriteLine("Çift Toplam: "+ciftToplam);
+
+            //* break, continue
+            for (int i = 1; i < 10; i++)
+            {
+                if(i%2==5)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i%2==5)
+                    continue;
+                Console.WriteLine(i);    
+            }
+
+
+
+
+
+
+
+
+
+
+
 
      
         }
